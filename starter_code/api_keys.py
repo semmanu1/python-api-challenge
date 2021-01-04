0919e7c358c3ad6be96c4c77dccfc22f011c184f
Samori Emmanuel <semmanu1@binghamton.edu>
2021-01-04 13:13:26 -0500

    made a few changes and updated api keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "259f325b73c5598cc84b6040b0f5eaa8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB8dR0vjmWmjosDiYr_XmuZuG6fB7C0gzs"
